100eb157b7071a474489480f304ee5bc17619806
Albert <albertveldman96@gmail.com>
2017-04-04 13:28:57 +0200

    Added redux store

--- scorebord/src/index.js
+++ scorebord/src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+import MainContainer from './containers/MainContainer';
 import './index.css';
 
+let store;
+const middleware = applyMiddleware(
+    thunk
+);
+
+if (window.__REDUX_DEVTOOLS_EXTENSION__) {
+    const reduxDevToolConnector = window.__REDUX_DEVTOOLS_EXTENSION__();
+    store = createStore(
+        reducers,
+        compose(middleware, reduxDevToolConnector)
+    );
+} else {
+    console.log('Redux dev tool is er niet pleb!');
+    store = createStore(
+        reducers,
+        middleware
+    );
+}
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <MainContainer/>
+    </Provider>,
+    document.getElementById('root')
 );
