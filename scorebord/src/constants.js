8d91be0e2ad78a9452d470d35bc6fc08c3686cb5
Albert <albertveldman96@gmail.com>
2017-04-06 10:51:09 +0200

    Updated constants

--- scorebord/src/constants.js
+++ scorebord/src/constants.js
@@ -1,9 +1,19 @@
 // PAGES
+export const REGISTER = 'register';
 export const SHOW_CODE = 'show code';
 export const QUIZ = 'quiz';
 export const RESULTS = 'results';
 
 // ACTIONS
+export const PENDING_REGISTER = 'pending register';
+export const RECEIVE_REGISTER = 'receive register';
+export const PENDING_QUESTION = 'pending question';
+export const RECEIVE_QUESTION = 'receive question';
 export const START_QUIZ = 'start quiz';
 export const FINISH_QUIZ = 'finish quiz';
+export const START_QUESTION = 'start question';
+export const STOP_QUESTION = 'stop question';
+export const SHOW_RESULTS = 'show results';
+export const RESET_QUESTION = 'reset question';
 export const ADD_TEAMS = 'add teams';
+export const ADD_SCORE = 'add score';
