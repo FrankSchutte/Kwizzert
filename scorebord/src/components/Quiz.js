023bff2dbe25a915d8a45b83a6621a45bc96fca3
Albert <albertveldman96@gmail.com>
2017-04-06 13:33:40 +0200

    Coupled Quiz screen to store

--- scorebord/src/components/Quiz.js
+++ scorebord/src/components/Quiz.js
@@ -2,6 +2,11 @@ import React, {Component, PropTypes} from 'react';
 
 class Quiz extends Component {
     render() {
+        const quizInfo =
+            <div>
+                <p>Vraag: {this.props.questionNum} Ronde: {this.props.roundNum}</p>
+            </div>;
+
         const teamInfo = this.props.teams.map((team) =>
             <div key={team.teamName}>
                 <ul>
@@ -11,14 +16,58 @@ class Quiz extends Component {
                 </ul>
             </div>
         );
-        console.log(teamInfo);
+
+        let question;
+        if (this.props.question && this.props.status !== 'invisible') {
+            question =
+                <div>
+                    <div>
+                        {this.props.question.category}
+                    </div>
+                    <div>
+                        {this.props.question.question}
+                    </div>
+                        {this.props.status === 'answer' ?
+                            <div>
+                                {this.props.question.answer}
+                            </div>
+                            : ''
+                        }
+                </div>
+        } else question = '';
+
+        let results;
+        if (this.props.results && this.props.status !== 'invisible') {
+            results = this.props.results.map((result) =>
+                <div key={result.teamName + '1'}>
+                    <br/>
+                    {result.teamName}
+                    <br/>
+                    {result.answer}
+                    <br/>
+                    {result.approved ? 'Goed' : 'Fout'}
+                </div>
+            );
+        } else results = '';
+
         return (
-            <div>{teamInfo}</div>
+            <div>
+                <div>{quizInfo}</div>
+                <div>{teamInfo}</div>
+                <hr/>
+                <div>{question}</div>
+                <div>{results}</div>
+            </div>
     )};
 }
 
 Quiz.propTypes = {
-    teams: PropTypes.array.isRequired
+    teams: PropTypes.array.isRequired,
+    question: PropTypes.object,
+    status: PropTypes.string.isRequired,
+    results: PropTypes.array,
+    questionNum: PropTypes.number.isRequired,
+    roundNum: PropTypes.number.isRequired
 };
 
 export default Quiz;
--- scorebord/src/containers/QuizContainer.js
+++ scorebord/src/containers/QuizContainer.js
@@ -2,7 +2,12 @@ import {connect} from 'react-redux';
 import Quiz from '../components/Quiz';
 
 const mapStateToProps = (state) => ({
-    teams: state.teamsReducer.teams
+    teams: state.teamsReducer.teams,
+    question: state.questionReducer.question,
+    status: state.questionReducer.status,
+    results: state.questionReducer.results,
+    questionNum: state.teamsReducer.questionNum,
+    roundNum: state.teamsReducer.roundNum
 });
 
 export default connect(mapStateToProps)(Quiz);
