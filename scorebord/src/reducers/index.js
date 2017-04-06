36145ad1711ea9f816400530a0133e77f1e921e9
Albert <albertveldman96@gmail.com>
2017-04-06 10:51:35 +0200

    Added new reducers to store

--- scorebord/src/reducers/index.js
+++ scorebord/src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import routingReducer from './routingReducer';
 import teamsReducer from './teamsReducers';
+import questionReducer from './questionReducer';
 
 const reducers = combineReducers({
     routingReducer,
-    teamsReducer
+    teamsReducer,
+    questionReducer
 });
 
 export default reducers;
