fd1887e382069ce5e1fa7c8a359c42a454bc5ee1
Albert <albertveldman96@gmail.com>
2017-04-07 15:09:40 +0200

    Removed I and l from possible characters in code

--- api.js
+++ api.js
@@ -114,7 +114,7 @@ api.get('/questions/:id', (req, res) => {
 
 const createCode = () => {
     let code = "";
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const characters = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
     for( let i=0; i < 6; i++ )
         code += characters.charAt(Math.floor(Math.random() * characters.length));
     return code;
